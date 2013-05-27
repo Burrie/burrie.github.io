65c4f54326e98ec6e7d58c11d8d8bbe1c762b522
Paul Burrie <burriep@gmail.com>
2013-05-27 15:12:34 -0500

    Fixed Mobile Menu

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function hideNav() {
-	if(document.getElementById("nav").style.display=="none") {
-		document.getElementById("nav").style.display="block";
-		document.getElementById("nav_cover").style.display="none";
+	if(document.getElementById("nav_mobile").style.display=="none") {
+		document.getElementById("nav_mobile").style.display="block";
+		document.getElementById("nav_mobile_toggle").style.backgroundColor="#4D4D4D";
 	}
 	else{
-		document.getElementById("nav").style.display="none";
-		document.getElementById("nav_cover").style.display="block";
+		document.getElementById("nav_mobile").style.display="none";
+		document.getElementById("nav_mobile_toggle").style.backgroundColor="#666";
 	}
 }
